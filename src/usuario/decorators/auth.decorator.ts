7b77e83cdb2d31255283007e2a39bf755a5b6335
Manuel <mrmanuelalvarez@gmail.com>
2026-01-09 22:08:16 -0300

    guard to manage tokens on its type

--- src/usuario/decorators/auth.decorator.ts
+++ src/usuario/decorators/auth.decorator.ts
@@ -3,10 +3,11 @@ import { AuthGuard } from '@nestjs/passport';
 import { UserValidRoleGuard } from '../guards/user-valid-role.guard';
 import { ValidRoles } from '../enums/usuario.enum';
 import { RoleProtected } from './role-protected.decorator';
+import { TokenTypeGuard } from '../guards/token-type.guard';
 
 export function Auth(...roles: ValidRoles[]) {
   return applyDecorators(
     RoleProtected(...roles),
-    UseGuards(AuthGuard(), UserValidRoleGuard),
+    UseGuards(AuthGuard(), UserValidRoleGuard, TokenTypeGuard),
   );
 }
