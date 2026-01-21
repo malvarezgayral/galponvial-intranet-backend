b65a78fd2868ca7b88c848c4c292fd244cab9fec
milagros alvarez <milagrosalvarez2604@gmail.com>
2026-01-21 16:40:03 -0300

    tests(almacen): fix auth in tests, almost completed

--- test/almacen.e2e-spec.ts
+++ test/almacen.e2e-spec.ts
@@ -1,48 +1,72 @@
-import { INestApplication } from '@nestjs/common';
+import { ExecutionContext, INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { typeOrmAlmacenTestConfig } from '../src/database/test/typeorm-almacen.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_GUARD } from '@nestjs/core';
 import request from 'supertest';
 
+import { typeOrmAlmacenTestConfig } from '../src/database/test/typeorm-almacen.config';
 import { AlmacenModule } from '../src/almacen/almacen.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { generateTestToken } from './utils/jwt-test.util';
+import { UserValidRoleGuard } from 'src/usuario/guards/user-valid-role.guard';
+
+// Esta clase "engaña" a Passport registrándose como 'jwt-access'
 
 describe('Almacen E2E', () => {
   let app: INestApplication;
-  let jwtService: JwtService;
-  let token: string;
 
-  beforeAll(async () => {
+beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
       imports: [
         TypeOrmModule.forRoot(typeOrmAlmacenTestConfig),
-        JwtModule.register({
-        secret: process.env.JWT_SECRET,
-        signOptions: { expiresIn: Number(process.env.JWT_EXPIRES_IN) || 3600 },
-        }),
         AlmacenModule,
       ],
-    }).compile();
+      providers: [
+        {
+            provide: APP_GUARD,
+            useValue: { canActivate: () => true }
+        }
+      ],
+    })
+      // --- AQUI ESTA LA MAGIA ---
+      
+      // 1. Sobrescribimos el Guard de JWT (La puerta de entrada)
+      .overrideGuard(UserValidRoleGuard) // <--- Asegúrate de usar la clase correcta que importaste
+      .useValue({
+        canActivate: (context: ExecutionContext) => {
+          const req = context.switchToHttp().getRequest();
+          // Inyectamos el usuario "a la fuerza" en la request
+          req.user = { 
+            id: 1, 
+            dni: '12345678', 
+            rol: { rol: 'ADMIN' } 
+          };
+          return true; // Dejar pasar siempre
+        },
+      })
+      
+      // 2. Sobrescribimos el Guard de Roles (por si acaso)
+      .overrideGuard(UserValidRoleGuard)
+      .useValue({ canActivate: () => true })
+      
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
 
-    jwtService = moduleFixture.get(JwtService);
-    token = generateTestToken(jwtService);
-  }, 20000);
+  afterAll(async () => {
+    await app.close();
+  });
 
   it('POST /almacen/articulos', async () => {
     const res = await request(app.getHttpServer())
       .post('/almacen/articulos')
-      .set('Authorization', `Bearer ${token}`)
       .send({
         nombre: 'Articulo Test',
         modelo: 'Modelo',
         descripcion: 'Descp',
         img_url: 'https://example.com/filtro.jpg',
         unidad_tipo: 'pieza',
-        cod_proveedor: "COD-1", 
+        cod_proveedor: 'COD-1',
       })
       .expect(201);
 
@@ -52,27 +76,25 @@ describe('Almacen E2E', () => {
   it('PUT /almacen/articulos', async () => {
     const resArticle = await request(app.getHttpServer())
       .post('/almacen/articulos')
-      .set('Authorization', `Bearer ${token}`)
       .send({
         nombre: 'Articulo Test',
         modelo: 'Modelo',
         descripcion: 'Descp',
         img_url: 'https://example.com/filtro.jpg',
         unidad_tipo: 'pieza',
-        cod_proveedor: "COD-1",
+        cod_proveedor: 'COD-1',
       })
       .expect(201);
 
     const resPut = await request(app.getHttpServer())
       .put(`/almacen/articulos/${resArticle.body.cod}`)
-      .set('Authorization', `Bearer ${token}`)
       .send({
         nombre: 'Articulo Test Actualizado',
         descripcion: 'Descp',
         modelo: 'Nuevo modelo',
         stock: 9,
         unidad_tipo: 'pieza',
-        cod_proveedor: "COD-2",
+        cod_proveedor: 'COD-2',
       })
       .expect(200);
 
@@ -82,7 +104,6 @@ describe('Almacen E2E', () => {
   it('GET /almacen/articulos', async () => {
     const res = await request(app.getHttpServer())
       .get('/almacen/articulos')
-      .set('Authorization', `Bearer ${token}`)
       .expect(200);
 
     expect(Array.isArray(res.body)).toBe(true);
@@ -91,7 +112,6 @@ describe('Almacen E2E', () => {
   it('POST /almacen/grupos', async () => {
     const res = await request(app.getHttpServer())
       .post('/almacen/grupos')
-      .set('Authorization', `Bearer ${token}`)
       .send({
         nombre: 'Grupo Test',
         descripcion: 'Descp grupo',
@@ -105,7 +125,6 @@ describe('Almacen E2E', () => {
   it('POST /almacen/movimientos - should create salida', async () => {
     const resArticle = await request(app.getHttpServer())
       .post('/almacen/articulos')
-      .set('Authorization', `Bearer ${token}`)
       .send({
         nombre: 'Articulo Test',
         modelo: 'Modelo',
@@ -113,18 +132,17 @@ describe('Almacen E2E', () => {
         img_url: 'https://example.com/filtro.jpg',
         unidad_tipo: 'pieza',
         grupo_id: 1,
-        cod_proveedor: "COD-2", 
+        cod_proveedor: 'COD-2',
       })
       .expect(201);
 
     const resMovement = await request(app.getHttpServer())
       .post('/almacen/movimientos')
-      .set('Authorization', `Bearer ${token}`)
       .send({
         tipo: 'perdida',
         detalle: 'se rompio algo',
         motivo_salida: 'pos eso mismo',
-        cod_articulo: Number(resArticle.body.cod), // aseguramos que sea number
+        cod_articulo: Number(resArticle.body.cod),
       })
       .expect(201);
 
