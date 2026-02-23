4930d64a17747981d380b2572c82ff6b69872a3b
milagros alvarez <milagrosalvarez2604@gmail.com>
2026-02-23 09:34:56 -0300

    replace with real type of vehicle enum

--- src/vehiculos/enums/vehiculo.enum.ts
+++ src/vehiculos/enums/vehiculo.enum.ts
@@ -6,12 +6,23 @@ export enum VehiculoStatus {
 }
 
 export enum TipoVehiculo {
-  AUTOMOVIL = 'automovil',
-  CAMION = 'camion',
-  CAMIONETA = 'camioneta',
-  RETROEXCAVADORA = 'retroexcavadora',
-  MAQUINARIA = 'maquinaria',
-  MOTOCICLETA = 'motocicleta',
+  TRASLADO = 'TRASLADO',
+  PLAYO = 'PLAYO',
+  RETRO_A_ORUGAS = 'RETRO A ORUGAS',
+  VOLCADOR = 'VOLCADOR',
+  COLECTIVO = 'COLECTIVO',
+  HIDROGRUA = 'Hidrogrúa',
+  MOTONIVELADORA = 'MOTONIVELADORA',
+  PALA_RETRO = 'PALA / RETRO',
+  TRACTOR = 'TRACTOR',
+  AMBULANCIA = 'AMBULANCIA',
+  CUATRO_X_CUATRO = '4X4',
+  PALA_FRONTAL = 'PALA FRONTAL',
+  RIEGO = 'RIEGO',
+  MINIBUS = 'MINIBUS',
+  COMPACTADOR_RESIDUOS = 'COMPACTADOR RESIDUOS',
+  CAMION = 'CAMION',
+  AUTO = 'AUTO',
   OTRO = 'otro',
 }
 
