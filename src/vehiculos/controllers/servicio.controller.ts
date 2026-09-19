dbdef501a322263b0df415a0885b3c6bbfe53337
david20262027 <daviddaruich2026@gmail.com>
2026-09-19 11:59:54 +0000

    fix: agregar @Auth a servicio.controller (rutas /servicios estaban sin proteccion)

--- src/vehiculos/controllers/servicio.controller.ts
+++ src/vehiculos/controllers/servicio.controller.ts
@@ -10,12 +10,15 @@ import {
 import { ServicioService } from '../services/servicio.service';
 import { CreateServicioDto } from '../dto/create-servicio.dto';
 import { ServicioResponseDto } from '../dto/servicio-response.dto';
+import { Auth } from 'src/usuario/decorators/auth.decorator';
+import { ValidRoles } from 'src/usuario/enums/usuario.enum';
 
 @Controller('servicios')
 export class ServicioController {
   constructor(private readonly servicioService: ServicioService) {}
 
   @Post()
+  @Auth(ValidRoles.admin, ValidRoles.superadmin)
   @HttpCode(HttpStatus.CREATED)
   create(
     @Body() createServicioDto: CreateServicioDto,
@@ -24,16 +27,19 @@ export class ServicioController {
   }
 
   @Get()
+  @Auth()
   findAll(): Promise<ServicioResponseDto[]> {
     return this.servicioService.findAll();
   }
 
   @Get(':id')
+  @Auth()
   findOne(@Param('id') id: string): Promise<ServicioResponseDto> {
     return this.servicioService.findOne(+id);
   }
 
   @Get('incidente/:idIncidente')
+  @Auth()
   findByIncidente(
     @Param('idIncidente') idIncidente: string,
   ): Promise<ServicioResponseDto[]> {
@@ -41,6 +47,7 @@ export class ServicioController {
   }
 
   @Get('vehiculo/:idVehiculo')
+  @Auth()
   findByVehiculo(
     @Param('idVehiculo') idVehiculo: string,
   ): Promise<ServicioResponseDto[]> {
