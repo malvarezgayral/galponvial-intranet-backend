a658b929b24f26ffa6b2b55877f2e49ed7545f57
david20262027 <daviddaruich2026@gmail.com>
2026-09-20 17:29:59 +0000

    feat(notificaciones): endpoint GET /notificaciones/no-leidas con conteo por tipo (personal solo superadmin)

--- src/notificaciones/controllers/notificaciones.controller.ts
+++ src/notificaciones/controllers/notificaciones.controller.ts
@@ -3,10 +3,19 @@ import { Controller, Get, Patch, Param, Query, ParseIntPipe } from '@nestjs/comm
 import { NotificacionesService } from '../services/notificaciones.service';
 import { Auth } from '../../usuario/decorators/auth.decorator';
 import { ValidRoles } from '../../usuario/enums/usuario.enum';
+import { GetUser } from '../../usuario/decorators/get-user.decorator';
+import { Usuario } from '../../usuario/entities/usuario.entity';
 
 @Controller('notificaciones')
 export class NotificacionesController {
   constructor(private readonly notificacionesService: NotificacionesService) {}
+
+  @Get('no-leidas')
+  @Auth(ValidRoles.admin, ValidRoles.superadmin)
+  contarNoLeidas(@GetUser() user: Usuario) {
+    const esSuperadmin = user.roles.some((r) => r.rol === ValidRoles.superadmin);
+    return this.notificacionesService.contarNoLeidasPorTipo(esSuperadmin);
+  }
 
   @Get()
   @Auth(ValidRoles.admin, ValidRoles.superadmin)
